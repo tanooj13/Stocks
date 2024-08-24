130c9e026cc5bcbe9f19b1ef2da368ccdceb21d0
Tanooj Reddy <142309781+tanooj13@users.noreply.github.com>
2024-08-24 18:28:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -17,6 +17,8 @@ click = st.button("predict", type="primary")
 with open('model.pkl','rb') as f:
     model = pickle.load(f)
 
-
+X = [[ 230,  230, 230 ,165428800  , 0.    ,      0. , 240 , 220 , 1.        ]]
 if (click):
-    st.write(model.predict())
+    st.write("Model Predicted:")
+    st.write(model.predict(X))
+else:st.write("Select a company")
